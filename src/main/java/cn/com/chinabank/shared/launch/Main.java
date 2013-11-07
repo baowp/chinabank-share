e66a95598024359fd2db1fec509230fb640e6a6a
baowp <122515909@qq.com>
2013-11-07 22:16:04 +0800

    a

---
+++ src/main/java/cn/com/chinabank/shared/launch/Main.java
@@ -0,0 +1,79 @@
+package cn.com.chinabank.shared.launch;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: baowp
+ * Date: 11/7/13
+ * Time: 10:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static final String CONTAINER_KEY = "dubbo.container";
+
+    public static final String SHUTDOWN_HOOK_KEY = "dubbo.shutdown.hook";
+
+    //private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    //private static final ExtensionLoader<Container> loader = ExtensionLoader.getExtensionLoader(Container.class);
+
+    private static volatile boolean running = true;
+
+    public static void main(String[] args) {
+        try {
+            /*if (args == null || args.length == 0) {
+                String config = ConfigUtils.getProperty(CONTAINER_KEY, loader.getDefaultExtensionName());
+                args = Constants.COMMA_SPLIT_PATTERN.split(config);
+            }*/
+
+            final List<Container> containers = new ArrayList<Container>();
+            containers.add(new SpringContainer());
+            containers.add(new JettyContainer());
+           /* for (int i = 0; i < args.length; i ++) {
+                containers.add(loader.getExtension(args[i]));
+            }
+            System.out.println("Use container type(" + Arrays.toString(args) + ") to run dubbo serivce.");
+
+            if ("true".equals(System.getProperty(SHUTDOWN_HOOK_KEY))) {
+                Runtime.getRuntime().addShutdownHook(new Thread() {
+                    public void run() {
+                        for (Container container : containers) {
+                            try {
+                                container.stop();
+                                System.out.println("Dubbo " + container.getClass().getSimpleName() + " stopped!");
+                            } catch (Throwable t) {
+                                logger.error(t.getMessage(), t);
+                            }
+                            synchronized (Main.class) {
+                                running = false;
+                                Main.class.notify();
+                            }
+                        }
+                    }
+                });
+            }
+*/
+            for (Container container : containers) {
+                container.start();
+                System.out.println("Dubbo " + container.getClass().getSimpleName() + " started!");
+            }
+            System.out.println(new SimpleDateFormat("[yyyy-MM-dd HH:mm:ss]").format(new Date()) + " Dubbo service server started!");
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            //logger.error(e.getMessage(), e);
+            System.exit(1);
+        }
+        synchronized (Main.class) {
+            while (running) {
+                try {
+                    Main.class.wait();
+                } catch (Throwable e) {
+                }
+            }
+        }
+    }
+}
