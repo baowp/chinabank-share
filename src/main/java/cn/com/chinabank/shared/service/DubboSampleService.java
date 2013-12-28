c0d98e212272690550312db545b56892a057b33d
baowp <122515909@qq.com>
2013-12-28 16:08:04 +0800

    modified

--- src/main/java/cn/com/chinabank/shared/service/DubboSampleService.java
+++ src/main/java/cn/com/chinabank/shared/service/DubboSampleService.java
@@ -1,8 +1,12 @@
 package cn.com.chinabank.shared.service;
 
+import cn.com.chinabank.shared.dto.BookDto;
+
 import java.io.Serializable;
 
 public interface DubboSampleService {
 
 	Serializable execute(String s);
+
+    Object execute(BookDto book);
 }
